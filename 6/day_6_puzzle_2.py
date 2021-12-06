477b6026a111e734cd2c9e605b2851e7f35aaa48
Sam Humby <samalotmedia@gmail.com>
2021-12-06 21:15:09 +0000

    day 6

--- 6/day_6_puzzle_2.py
+++ 6/day_6_puzzle_2.py
@@ -2,7 +2,7 @@ import Reader
 
 
 def run():
-    # Counts the total amount of each fish at a given timer
+    # Count the total amount of each fish at a given timer
     counts = [0] * 9
 
     # Read in seed data
@@ -13,15 +13,15 @@ def run():
     # Loop for 256 days
     for day in range(256):
         # Save the number of fish currently reproducing
-        numer_of_zeros = counts[0]
+        number_of_zeros = counts[0]
 
         # Shift all other fish down a cycle
         for i in range(1, len(counts)):
             counts[i-1] = counts[i]
 
         # Reset the 0 fish and add new batch of fish
-        counts[8] = numer_of_zeros
-        counts[6] += numer_of_zeros
+        counts[8] = number_of_zeros
+        counts[6] += number_of_zeros
 
     return sum(counts)
 
